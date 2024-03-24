319778bb39a76ac3e8202a9a5485b007baaee286
strix <sartsartsartsartsartsart@gmail.com>
2024-03-24 21:45:53 +0300

    9. Площадь треугольника. [#175474 #28062]

---
+++ src/main/java/ru/job4j/TriangleArea.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class TriangleArea {
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        double four = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return four;
+    }
+
+    public static void main(String[] args) {
+        double result = TriangleArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + result);
+    }
+}
